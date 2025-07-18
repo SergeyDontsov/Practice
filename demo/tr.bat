eef5937fa36cd99f1cb5c8071400229f714851fc
SergeyDontsov <dontsovserezhenka@gmail.com>
2025-07-18 19:02:58 +0300

    Версия №3

--- demo/tr.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set JAVA_LIB_PATH=C:\Users\1\Documents\Practice-native\demo
-
-java --enable-native-access=ALL-UNNAMED -Djava.library.path=%JAVA_LIB_PATH% -jar test.jar -a max -f number -g name -d "C:\Users\1\Documents\Practice-native\nabor(group).json"
-pause
